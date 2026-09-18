417f48d2d5254248d3f524f2241dd700e0ded2ab
Eduardo Martínez Catalá <3d24rd0@gmail.com>
2026-09-18 08:32:59 +0200

    refactor: remove flutter material and cupertino imports from lib in favor of widgets

--- lib/modal_route/browser_page_route.dart
+++ lib/modal_route/browser_page_route.dart
@@ -1,7 +1,5 @@
 import 'package:browser_router/gestures/swipe/swipe.dart';
-import 'package:flutter/cupertino.dart' show CupertinoRouteTransitionMixin;
 import 'package:flutter/foundation.dart'; // Import for debugPrint
-import 'package:flutter/material.dart' show MaterialRouteTransitionMixin;
 import 'package:flutter/widgets.dart';
 
 import '../browser.dart';
@@ -64,11 +62,7 @@ class BrowserPageRoute<T> extends PageRoute<T>
   @override
   bool canTransitionTo(TransitionRoute<dynamic> nextRoute) {
     // Don't perform outgoing animation if the next route is a fullscreen dialog.
-    return (nextRoute is MaterialRouteTransitionMixin &&
-            !nextRoute.fullscreenDialog) ||
-        (nextRoute is CupertinoRouteTransitionMixin &&
-            !nextRoute.fullscreenDialog) ||
-        (nextRoute is BrowserPageRoute && !nextRoute.fullscreenDialog);
+    return nextRoute is PageRoute<dynamic> && !nextRoute.fullscreenDialog;
   }
 
   @override
